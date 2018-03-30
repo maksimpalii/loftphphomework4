a3c96c5d9ae89c479ef30bd09c246879c4ba832e
Maksim Palii <maksim.palii@gmail.com>
2018-03-30 22:03:38 +0300

    git

---
+++ EngineWorkIntrface.php
@@ -0,0 +1,15 @@
+<?php
+
+interface EngineWorkIntrface
+{
+    public function engine();
+
+    public function engineStart();
+
+    public function engineStop();
+
+    public function engineWork();
+
+    public function engineOff();
+
+}
